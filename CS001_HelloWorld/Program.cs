711b55238cb6db03f0e9c0e9aa4c3f545d36e309
haxxu <nguyenngocminh@190901@gmail.com>
2024-03-28 17:51:52 +0700

    update

--- CS001_HelloWorld/Program.cs
+++ CS001_HelloWorld/Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine("Hello, World!");
 
 
+// Khai báo biến và gán ngay giá trị lưu vào biến
+string address = "Hà Nội, Việt Nam";
+
+// Khai báo biến, sau đó trước khi sử dụng gán giá trị cho biến
+int studentAge;
+studentAge = 22;
+
+Con
